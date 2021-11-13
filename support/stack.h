ead6f32f902c3459c3375c90eab1c7936c51fce1
Philippe Marguinaud <pmarguinaud@hotmail.com>
2021-11-13 18:52:10 +0100

    Works with stack

--- support/stack.h
+++ support/stack.h
@@ -5,7 +5,7 @@
 
 #define temp(t, n, s) t, DIMENSION s :: n; POINTER (IP_##n##_, n)
 
-#define alloc(n) IP_##n##_=LOC(YLSTACK%L);YLSTACK%L=YLSTACK%L+8*SIZE(n);IF(YLSTACK%L>YLSTACK%U)CALL ABOR1("")
+#define alloc(n) IP_##n##_=YLSTACK%L;YLSTACK%L=YLSTACK%L+8*SIZE(n);IF(YLSTACK%L>YLSTACK%U)CALL ABOR1(__FILE__)
 
 #else
 
